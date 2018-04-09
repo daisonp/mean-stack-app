8a7cea2da5f8e1de30119d10ecae2f9dd907e4b5
Richard Parry <richard.w.parry@gmail.com>
2018-04-09 12:57:42 +0100

    added express

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+
+app.set('port', 3000);
+
+// app.use('/public',express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+/*
+// app.use removes the need to define a default route
+app.get('/', function(req, res) {
+    console.log("Get the homepage");
+    res
+      .status(200)
+      .sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+*/
+
+app.get('/json', function(req, res) {
+    console.log("Get the json");
+    res
+      .status(200)
+      .json({"jsonData": true});
+});
+
+app.get('/file', function(req, res) {
+    console.log("Get the file");
+    res
+      .status(200)
+      .sendFile(path.join(__dirname, 'app.js'));
+});
+
+var server = app.listen(app.get('port'), function() {
+    var port = server.address().port;
+    console.log('Magic happens on port ' +port);
+});
+
+
