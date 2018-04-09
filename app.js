9e459230b3f2ad202e5340d65c9c97d7be807cc7
Richard Parry <richard.w.parry@gmail.com>
2018-04-09 19:20:21 +0100

    adding routes to express

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+var routes = require('./routes');
+
 app.set('port', 3000);
 
 app.use(function(req, res, next) {
@@ -10,24 +12,7 @@ app.use(function(req, res, next) {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-/*
-// app.use removes the need to define a default route
-app.get('/', function(req, res) {
-    console.log("Get the homepage");
-    res
-      .status(200)
-      .sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-*/
-
-app.get('/json', function(req, res) {
-    console.log("Get the json");
-    res
-      .status(200)
-      .json({"jsonData": true});
-});
+app.use('/api', routes);
 
 app.get('/file', function(req, res) {
     console.log("Get the file");
---
+++ routes/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+
+router
+  .route('/json')
+  .get( function(req, res) {
+    console.log("Get the json");
+    res
+      .status(200)
+      .json({"jsonData": true});
+  })
+  .post( function(req, res) {
+    console.log("Post the json");
+    res
+      .status(200)
+      .json({"jsonData": "POST received"});
+  });
+
+
+module.exports = router;
