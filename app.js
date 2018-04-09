b95c58cbd8625bcb639851cc09a42ff62d6d3511
Richard Parry <richard.w.parry@gmail.com>
2018-04-09 14:03:05 +0100

    express middleware

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var path = require('path');
 
 app.set('port', 3000);
 
-// app.use('/public',express.static(path.join(__dirname, 'public')));
+app.use(function(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 /*
